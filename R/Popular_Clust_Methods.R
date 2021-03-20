e1e9cc0150ae8995648d1717a0e235bdfc483787
Timothy Su <kenongsu@Timothys-MacBook-Pro.local>
2021-03-20 00:36:42 -0400

    fix subscribe error

--- R/Popular_Clust_Methods.R
+++ R/Popular_Clust_Methods.R
@@ -13,11 +13,6 @@
 #' @param k The number of clusters. If it is not provided, k is estimated by the default method in SC3.
 #' @param input_markers A character vector including the featured genes. If they are not presented, SC3 will take care of this.
 #' @return the clustering labels and the featured genes.
-#' @examples
-#' data(Yan)
-#' k = length(unique(trueclass))
-#' res = SC3_Clust(Y, k=k) # could specific the input features by using input_markers argument
-#' @export
 SC3_Clust = function(Y, k = NULL, input_markers = NULL){
     # Y is the count matrix
     sce = SingleCellExperiment(
